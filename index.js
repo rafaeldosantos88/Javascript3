772e8e3a942ee53d62bd261316dceebba3da902c
Rafael Dos Santos <raphael_979@hotmail.com>
2019-11-19 22:32:25 -0300

    console.log(); imprimindo algumas mensagens no console

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('Rafael dos santos');   //Sempre que colocar um valor literal console.log('Rafael').
+console.log("Eu sou Corinthiano");
+
+console.log(123456789);
+console.log(12, 15.82,'Rafael')//Exebindo número com Ponto flutuante em javascript.Não quebra a linha no caso
+//É tudo number em Javascript não tem separação de ponto flutuante ou double
+
